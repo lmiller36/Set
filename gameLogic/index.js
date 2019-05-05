63e98416e4ba2cd1d4cc6f2de160f9caef89689f
Lorne Miller <lmiller36@wisc.edu>
2019-05-05 14:40:51 +0200

    High score work

---
+++ gameLogic/index.js
@@ -0,0 +1,38 @@
+
+var express = require("express");
+var app = express();
+var path = require("path");
+
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+
+
+
+// app.listen(3000);
+// console.log("Server running at Port 3000");
+
+// var express = require("express");
+// var app = express();
+// var path = require("path");
+
+//app.use(express.static('public'));
+
+//Serves all the request which includes /images in the url from Images folder
+// app.use('/imgs', express.static(__dirname + '/imgs'));
+// app.use('/style', express.static(__dirname + '/style'));
+// app.use('/shapes', express.static(__dirname + '/shapes'));
+// app.use('/googleSheets', express.static(__dirname + '/googleSheets'));
+
+app.get('/',function(req,res){
+	res.sendFile(path.join(__dirname+'/index.html'));
+	app.use("/public", express.static(__dirname + "/public"));
+	app.use(express.static(__dirname + 'public'))
+	app.use('/imgs', express.static(__dirname + '/imgs'));
+	app.use('/style', express.static(__dirname + '/style'));
+	app.use('/shapes', express.static(__dirname + '/shapes'));
+	app.use('/googleSheets', express.static(__dirname + '/googleSheets'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
+var server = app.listen(3000);
+console.log("Server running at Port 3000");
